ec0feb4f2d0ba9911fa93fcf0740e1b46b4d2810
ThuPhuong1010 <phuongntt1010@gmail.com>
2021-11-12 04:08:04 +0700

    update_blog_1

---
+++ BLOG/frontend/js/carousel.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    
+    $("#news-slider2").owlCarousel({
+        items:3,
+        itemsDesktop:[1199,2],
+        itemsDesktopSmall:[980,2],
+        itemsMobile:[600,1],
+        pagination:true,
+        navigationText:false,
+        autoPlay:true,
+        loop:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true
+    });
+        
+    $("#news-slider3").owlCarousel({
+        items:3,
+        itemsDesktop:[1199,2],
+        itemsDesktopSmall:[980,2],
+        itemsMobile:[600,1],
+        pagination:true,
+        navigationText:false,
+        autoPlay:true,
+        loop:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true
+    });
+    $("#news-slider4").owlCarousel({
+        items:3,
+        itemsDesktop:[1199,2],
+        itemsDesktopSmall:[980,2],
+        itemsMobile:[600,1],
+        pagination:true,
+        navigationText:false,
+        autoPlay:true,
+        loop:true,
+        autoplayTimeout:1000,
+        autoplayHoverPause:true
+    });
+});
+
+
+
